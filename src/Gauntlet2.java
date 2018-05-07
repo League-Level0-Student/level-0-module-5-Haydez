e16f29ec1a3146870371494549f1c3ae1d9ef9ca
league <league@wts8.attlocal.net>
2018-05-06 17:23:00 -0700

    Gauntlet part 2, Skill practice

--- src/Gauntlet2.java
+++ src/Gauntlet2.java
@@ -1,23 +1,40 @@
 
 public class Gauntlet2 {
-public static void main(String[] args) {
-	for(int i=0; i<4; i++) {
-		for(int a=0; a<4; a++) {
-		System.out.println(i+"  "+a);
+	public static void main(String[] args) {
+		for (int i = 0; i < 4; i++) {
+			for (int a = 0; a < 4; a++) {
+				System.out.println(i + " " + a);
+			}
 		}
-	}
-	
-	for(int p=1; p<8; p+=3 ) {
-		for(int u=0; u<3; u++ ) {
-			
-				System.out.print(p+u+"   ");
-			
+
+		for (int p = 1; p < 8; p += 3) {
+			for (int u = 0; u < 3; u++) {
+
+				System.out.print(p + u + " ");
+
+			}
+			System.out.print("\n");
 		}
-	System.out.print("\n");
+
+		for (int k = 0; k < 101; k += 10) {
+			for (int g = 1; g < 11; g++) {
+
+				System.out.print(k + g + " ");
+			}
+			System.out.print("\n");
+		}
+
+		for (int l = 1; l < 7; l++) {
+			for (int m = 0; m < l; m++) {
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+
+		
+			for (int s = 100; s > 0 ;s--) {
+				System.out.println(s+"   ");
+			}
+		
 	}
-	
-		
-		
-	
-}
 }
---
+++ src/SkillWS.java
@@ -0,0 +1,11 @@
+
+public class SkillWS {
+public static void main(String[] args) {
+	SkillWS skills = new SkillWS();
+	skills.skill1();
+	skills.skill2();
+	skills.skill3();
+	skills.skill4();
+	skills.skill5();
+}
+}
